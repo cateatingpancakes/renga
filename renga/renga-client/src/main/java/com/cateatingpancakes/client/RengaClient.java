f8a8aa67a1d6c5b28dd945acdbae69866fc9a057
Mihnea <mihneaspiridon@gmail.com>
2026-04-03 06:53:01 +0300

    Set up Maven

---
+++ renga/renga-client/src/main/java/com/cateatingpancakes/client/RengaClient.java
@@ -0,0 +1,13 @@
+package com.cateatingpancakes.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class RengaClient 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ renga/renga-server/src/main/java/com/cateatingpancakes/server/RengaServer.java
@@ -0,0 +1,13 @@
+package com.cateatingpancakes.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class RengaServer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
